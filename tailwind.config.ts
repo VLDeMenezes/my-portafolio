1657f9ddff238360ab1248beaaa9cf5f9bdeb624
VLDeMenezes <estudiodemenezes@gmail.com>
2024-08-08 12:02:33 -0300

    Tailwindcss config & theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,11 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "class", // Puedes cambiar a 'media' o 'class'
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx,css}",
   ],
   theme: {
     extend: {
@@ -13,8 +14,17 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        basicBg: "#e5e5e5",
+        basicFont: "#000000",
+        basicDarkBg: "#14213d",
+        basicDarkFont: "#ffffff",
+        detailColor: "#fca311",
+        hoverColor: "",
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
